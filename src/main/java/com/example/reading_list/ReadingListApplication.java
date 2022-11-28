b657aa02d31cf6e4451beb38bd257ef5e7749e3d
mfadhili <mfadhili@github.com>
2022-11-28 16:21:05 +0300

    initial setup

---
+++ src/main/java/com/example/reading_list/ReadingListApplication.java
@@ -0,0 +1,14 @@
+package com.example.reading_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Enables component sacanning and autoconfiguration
+public class ReadingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);// Bootstrap the application
+
+	}
+
+}
